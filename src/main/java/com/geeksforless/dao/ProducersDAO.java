33f80a3832bf90400e1ca751c6b77e413fb06013
mykola <mykola.khlopyk@gmail.com>
2023-08-06 14:52:51 +0300

    added DAOs interfaces

---
+++ src/main/java/com/geeksforless/dao/ProducersDAO.java
@@ -0,0 +1,10 @@
+package com.geeksforless.dao;
+
+
+import com.geeksforless.domain.entity.Producer;
+
+import java.util.List;
+
+public interface ProducersDAO {
+    List<Producer> getAll();
+}
---
+++ src/main/java/com/geeksforless/dao/SouvenirsDAO.java
@@ -0,0 +1,10 @@
+package com.geeksforless.dao;
+
+
+import com.geeksforless.domain.entity.Souvenir;
+
+import java.util.List;
+
+public interface SouvenirsDAO {
+    List<Souvenir> getAll();
+}
